814b1aae0abe479aa02ff3afc112e612f5673253
Farzan <67397870+farzan-dehbashi@users.noreply.github.com>
2022-12-07 17:14:58 -0500

    container-max-water

---
+++ questions/11-Container-With-Most-Water.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxArea(self, height):
+        l = 0
+        r = len(height) - 1
+        max_area = 0
+        while l < r:
+            area = min(height[l], height[r]) * (r-l)
+            if area > max_area:
+                max_area = area
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+        return max_area
